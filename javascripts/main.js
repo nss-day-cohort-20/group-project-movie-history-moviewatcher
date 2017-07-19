fc2da52800ba776b7c75e079ec8b012726b66030
Megan B <brown.m.j@outlook.com>
2017-07-19 10:45:15 -0500

    user can login and log out

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,12 +6,28 @@ let bootstrap = ('../lib/node_modules/bootstrap/dist/js/bootstrap.min.js');
 let userFactory = require('./user-factory');
 let firebase = require("./firebaseConfig");
 
-$("#auth-btn").click( function() {
+$("#logItIn").click( function() {
+	$("#logItIn").addClass("hideIt");
+	$(".messagePreLogin").addClass("hideIt");
 	userFactory.logInGoogle()
 	.then( (result) => {
 		let user = result.user.uid;
 		console.log("user", user);
 		// songController.loadSongsToDom();
-		$("#logItOut").removeClass("hidden");
+		$("#logItOut").removeClass("hideIt");
+		$(".messagePostLogin").removeClass("hideIt");
+	});
+});
+
+$("#logItOut").click( function() {
+	$("#logItOut").addClass("hideIt");
+	console.log("log-out clicked");
+	firebase.auth().signOut()
+	.then(function() {
+	location.reload(true);
+	$("#logItIn").removeClass("hideIt");
+	$(".messagePreLogin").removeClass("hideIt");
+	}).catch(function(error) {
+	// An error happened.
 	});
 });
